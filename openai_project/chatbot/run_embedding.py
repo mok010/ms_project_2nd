6c3434f10fc0bddacdd9d11a0e3068426ea6ab82
LeeHooyo <dldbsrb00619@gmail.com>
2025-07-15 15:13:49 +0900

    불필요한 폴더 제거 및 디렉토리 정리

--- openai_project/chatbot/run_embedding.py
+++
@@ -1,9 +0,0 @@
-from embedding_utils import process_pdf_and_build_index
-
-pdf_files = [
-    "C:/Users/EL57/Desktop/논문/논문3_trimmed.pdf",
-    "C:/Users/EL57/Desktop/논문/논문4_trimmed.pdf"
-]
-
-for pdf_path in pdf_files:
-    process_pdf_and_build_index(pdf_path)
